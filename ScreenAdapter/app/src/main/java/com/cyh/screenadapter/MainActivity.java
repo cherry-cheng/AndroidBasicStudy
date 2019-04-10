a96329b5b0b8dd9de572ff902c791381820fd53e
Cyh <hs747315545@gmail.com>
2019-04-10 23:03:24 +0800

    自定义像素适配、百分比适配、像素密度适配、刘海屏的处理

---
+++ ScreenAdapter/app/src/main/java/com/cyh/screenadapter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cyh.screenadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cyh.screenadapter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.percent_layout);
+    }
+}
---
+++ ScreenAdapter/app/src/main/java/com/cyh/screenadapter/density/MyDensityActivity.java
@@ -0,0 +1,14 @@
+package com.cyh.screenadapter.density;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.cyh.screenadapter.R;
+
+public class MyDensityActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.density_activity);
+    }
+}
